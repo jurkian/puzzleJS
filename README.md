085e46942cf0a82069de29c7a6f0686d63365ec7
Jakub Jurkian <jakub@jurkian.com>
2016-04-26 13:15:32 +0200

    Create directory structure and add Gulp tasks

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # puzzleJS
+Create puzzles from the uploaded image
+
+## Getting started
+
+1. Install [Node JS](http://nodejs.org/)
+2. Install [Gulp](http://gulpjs.com/) with `npm install -g gulp-cli` and [Bower](http://bower.io/) with `npm install -g bower`
+3. Install dependencies with `npm install` and `bower install`
+
+Run `gulp serve` to start server and begin developing
+
+Run `gulp build` to create a build
---
+++ gulpfile.babel.js
@@ -0,0 +1,150 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+import {stream as wiredep} from 'wiredep';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+gulp.task('styles', () => {
+	return gulp.src('app/styles/*.scss')
+		.pipe($.plumber())
+		.pipe($.sourcemaps.init())
+		.pipe($.sass.sync({
+			outputStyle: 'expanded',
+			precision: 10,
+			includePaths: ['.']
+		}).on('error', $.sass.logError))
+		.pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+		.pipe($.sourcemaps.write())
+		.pipe(gulp.dest('.tmp/styles'))
+		.pipe(reload({stream: true}));
+});
+
+gulp.task('scripts', () => {
+	return gulp.src('app/scripts/**/*.js')
+		.pipe($.plumber())
+		.pipe($.sourcemaps.init())
+		.pipe($.babel())
+		.pipe($.sourcemaps.write('.'))
+		.pipe(gulp.dest('.tmp/scripts'))
+		.pipe(reload({stream: true}));
+});
+
+function lint(files, options) {
+	return () => {
+		return gulp.src(files)
+			.pipe(reload({stream: true, once: true}))
+			.pipe($.eslint(options))
+			.pipe($.eslint.format())
+			.pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+	};
+}
+
+gulp.task('lint', lint('app/scripts/**/*.js'));
+
+gulp.task('html', ['styles', 'scripts'], () => {
+	return gulp.src('app/*.html')
+		.pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+		.pipe($.if('*.js', $.uglify()))
+		.pipe($.if('*.css', $.uncss({
+			html: ['app/*.html']
+		})))
+		.pipe($.if('*.css', $.cssnano()))
+		.pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+	return gulp.src('app/images/**/*')
+		.pipe($.cache($.image({
+			pngquant: true,
+			optipng: false,
+			zopflipng: true,
+			advpng: true,
+			jpegRecompress: true,
+			jpegoptim: true,
+			mozjpeg: true,
+			gifsicle: true,
+			svgo: true
+		})))
+		.pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', () => {
+	return gulp.src(require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
+		.concat('app/fonts/**/*'))
+		.pipe(gulp.dest('.tmp/fonts'))
+		.pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', () => {
+	return gulp.src([
+		'app/*.*',
+		'!app/*.html'
+	], {
+		dot: true
+	}).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('serve', ['styles', 'scripts', 'fonts'], () => {
+	browserSync({
+		notify: false,
+		port: 9000,
+		server: {
+			baseDir: ['.tmp', 'app'],
+			routes: {
+				'/bower_components': 'bower_components'
+			}
+		}
+	});
+
+	gulp.watch([
+		'app/*.html',
+		'app/images/**/*',
+		'.tmp/fonts/**/*'
+	]).on('change', reload);
+
+	gulp.watch('app/styles/**/*.scss', ['styles']);
+	gulp.watch('app/scripts/**/*.js', ['scripts']);
+	gulp.watch('app/fonts/**/*', ['fonts']);
+	gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('serve:dist', () => {
+	browserSync({
+		notify: false,
+		port: 9000,
+		server: {
+			baseDir: ['dist']
+		}
+	});
+});
+
+// inject bower components
+gulp.task('wiredep', () => {
+	gulp.src('app/styles/*.scss')
+		.pipe(wiredep({
+			ignorePath: /^(\.\.\/)+/
+		}))
+		.pipe(gulp.dest('app/styles'));
+
+	gulp.src('app/*.html')
+		.pipe(wiredep({
+			exclude: ['bootstrap-sass'],
+			ignorePath: /^(\.\.\/)*\.\./
+		}))
+		.pipe(gulp.dest('app'));
+});
+
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+	return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+
+gulp.task('default', ['clean'], () => {
+	gulp.start('build');
+});
