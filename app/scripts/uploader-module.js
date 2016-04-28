c9545d8ef148bd08ef3e165e0b69a9043fb33d9c
Jakub Jurkian <jakub@jurkian.com>
2016-04-28 19:02:53 +0200

    Handle image upload: file input or drag and drop

--- app/scripts/uploader-module.js
+++ app/scripts/uploader-module.js
@@ -1,6 +1,7 @@
 var Uploader = (function() {
 
-	var _dropZone = document.querySelector("#drop-zone");
+	var _dropZone = document.querySelector("#drop-zone"),
+			_uploadImageBtn = document.querySelector("#upload-image");
 	
 	var _addHover = function() {
 		_dropZone.classList.add("drag-over");
@@ -10,10 +11,30 @@ var Uploader = (function() {
 		_dropZone.classList.remove("drag-over");
 	};
 
-	var _handleDrop = function() {
+	var _handleDrop = function(e) {
 		e.preventDefault();
 		e.stopPropagation();
 
+		// Detect if user uploaded the image using drag and drop or file input method
+		var uploadedFile = '';
+
+		if (e.type === 'drop') {
+			uploadedFile = e.dataTransfer.files;
+		} else {
+			uploadedFile = _uploadImageBtn.files;
+		}
+
+		if (uploadedFile.length !== 1) {
+			alert('You can upload only 1 image. Try again');
+
+		} else {
+
+			if (uploadedFile[0].type.match('image.*')) {
+				// There is one file and it's an image
+				_generateImgPreview(uploadedFile[0]);
+			}
+		}
+		
 		_removeHover();
 	};
 
@@ -22,11 +43,24 @@ var Uploader = (function() {
 		return false;
 	};
 
+	var _generateImgPreview = function(file) {
+		var reader = new FileReader(),
+				img = document.createElement('img');
+
+		reader.onload = function() {
+			img.src = reader.result;
+		};
+
+		reader.readAsDataURL(file);
+		document.body.appendChild(img);
+	};
+
 	var init = function() {
-		_dropZone.ondragenter = _addHover;
-		_dropZone.ondragleave = _removeHover;
-		_dropZone.ondragover = _cancelDefault;
-		_dropZone.ondrop = _handleDrop;
+		_dropZone.addEventListener('dragenter', _addHover, false);
+		_dropZone.addEventListener('dragleave', _removeHover, false);
+		_dropZone.addEventListener('dragover', _cancelDefault, false);
+		_dropZone.addEventListener('drop', _handleDrop, false);
+		_uploadImageBtn.addEventListener('change', _handleDrop, false);
 	};
 
 	return {
