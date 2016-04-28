e937626278b574e2cd717d8c00e77cb665894313
Jakub Jurkian <jakub@jurkian.com>
2016-04-28 19:15:00 +0200

    Show warning if browser doesn't support new APIs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 (function() {
 
+	// Detect if browser supports drag and drop and FileReader APIs
+	if (typeof FileReader === 'undefined' && 'draggable' in document.createElement('span') === false ) {
+		alert('Make sure your browser is up to date. Consider using Chrome or Firefox.');
+	}
+
 	Uploader.init();
 
 })();
